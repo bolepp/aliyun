f925fd7953057d74f122a0b85bde105eef376bf0
mijianyou <mijianyou@thit.com.cn>
2018-04-13 15:06:20 +0800

    app.js

--- openys/app.js
+++ openys/app.js
@@ -5,15 +5,10 @@ var querystring = require('querystring');
 var iconv = require('iconv-lite');
 var bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
-
 var postData= {
-	
 	'accessToken':'at.89rfc4jf5nrapbqv6s5rmuwra8tcjtle-51biy2ki4b-1mg3dwh-3sqv1qxbq',
 	'source':'C03753323:1'
-	
 }
-
-
 var options={
 	hostname:'open.ys7.com',	
 	path:'/api/lapp/live/address/get',
@@ -23,11 +18,8 @@ var options={
 		'Host':'open.ys7.com',
 		'Content-Type':'application/x-www-form-urlencoded',
 		'accessToken':'at.89rfc4jf5nrapbqv6s5rmuwra8tcjtle-51biy2ki4b-1mg3dwh-3sqv1qxbq'
-       
-	
 	}
 }
-
 //设置跨域访问
 app.all('*', function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
@@ -37,16 +29,12 @@ app.all('*', function(req, res, next) {
    res.header("Content-Type", "application/json;charset=utf-8");
    next();
 });
-
-
-
-//写个接口123
+//提供接口
 app.post('/openys',function(exreq,exres){
 	console.log(exreq.body);
 	postData.accessToken=exreq.body.accessToken;
 	postData.source=exreq.body.sourceAppkey+':'+exreq.body.sourcePass;
-		var req= https.request(options,function (res) {
-			
+	var req= https.request(options,function (res) {			
 	console.log('Status:'+res.statusCode);
 	var datas = [];
     var size = 0;
@@ -62,25 +50,17 @@ app.post('/openys',function(exreq,exres){
 		//exres.json(buff.toString());
 		 exres.send(str);
 	})
-
-	
 })
 req.on('error',function(e){
 		console.log('异常'+e);
 	})
 var queryData=querystring.stringify(postData);
 req.write(queryData);
-
 	req.end();
-	
 });
-
 //配置服务端口
-var server = app.listen(3000, function () {
-
+var server = app.listen(80, function () {
 var host = server.address().address;
-
  var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('listening at http://%s:%s', host, port);
 })
